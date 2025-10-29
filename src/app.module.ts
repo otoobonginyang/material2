e3935fc092cd447e66d9089455f6c75db6e8a805
Oto-Obong Inyang <ucheoto4real@yahoo.com>
2025-10-29 15:49:14 +0100

    Add new changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,23 +7,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entiy/entity';
 import { Materialentity } from './material/entities/material.entity';
 import { Spareentity } from './spare/entities/spare.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 
 
 @Module({
-  imports: [UserModule,AuthModule, MaterialModule, SpareModule,
-  TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'uchena1064',
-    database: 'material2',
-  entities:  [ UserEntity, Materialentity, Spareentity ],
-    synchronize: true,
-  }),
-  AuthModule
-  ],
+  imports: [DatabaseModule ,UserModule,AuthModule, MaterialModule, SpareModule,AuthModule],
 })
 export class AppModule {}
   
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,24 +1,34 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserEntity } from "src/entiy/entity";
-import { Materialentity } from "src/material/entities/material.entity";
-import { Spareentity } from "src/spare/entities/spare.entity";
-
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Materialentity } from 'src/material/entities/material.entity';
+import { Spareentity } from 'src/spare/entities/spare.entity';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 @Module({
- imports: [
-  TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'uchena1064',
-    database: 'material2',
-  entities: [UserEntity, Materialentity, Spareentity],
-  synchronize: true,
-    })
- ],
- exports: [TypeOrmModule]
-})
+  imports: [
+    // Make config available in this module
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
 
+    // Configure TypeORM asynchronously using ConfigService
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [UserEntity, Materialentity, Spareentity],
+        ssl: { rejectUnauthorized: false }, // needed if you’re using Aiven
+        synchronize: true, // disable in production
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
 export class DatabaseModule {}
